21cafa3384dbbba658f34cab156a86c4caa97462
Joseph Garner <joseph.peter.garner@gmail.com>
2022-08-03 08:30:56 +1000

    feat(mongoose): updated error handling

--- src/db/connectToDatabase.ts
+++ src/db/connectToDatabase.ts
@@ -3,9 +3,17 @@ import mongoose from "mongoose";
 export const connectToDatabase = async () => {
   const uri = process.env.DATABASE_URI || "mock";
   const database = process.env.DATABASE || "database";
-  mongoose.connect(`mongodb://${uri}/${database}`, () => {
-    console.log("Connected to Database");
-  });
+  try {
+    mongoose.connect(`mongodb://${uri}/${database}`, () => {
+      console.log("Connected to Database");
+    });
+    mongoose.connection.on("error", (err) => {
+      console.log(err);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
   // console.log(mongoose.connection);
   // if (!mongoose.connection) {
   // }
