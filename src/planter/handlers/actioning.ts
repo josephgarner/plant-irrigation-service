00c6ecee5430936d89540b6db40807dc5ca59175
Joseph Garner <joseph.peter.garner@gmail.com>
2022-10-15 12:18:40 +1100

    Updating docker compose file for nginx

--- src/planter/handlers/actioning.ts
+++ src/planter/handlers/actioning.ts
@@ -4,7 +4,12 @@ import { CommandType } from "../../types";
 export const actioning = (planterID: string, command: CommandType) => {
   console.info(`Command has been actioned - ${planterID}`);
   commandQueue.updateOne(
-    { planterID: planterID, issuedCommand: command, sent: true },
+    {
+      planterID: planterID,
+      issuedCommand: command,
+      sent: true,
+      actioned: false,
+    },
     { $set: { actioned: true, dateUpdated: new Date() } },
     (error: any, writeOpResult: any) => {
       return writeOpResult;
